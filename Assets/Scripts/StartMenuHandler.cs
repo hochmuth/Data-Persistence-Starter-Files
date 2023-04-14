08e97309fc25940e31c21142687a73d9f635aa02
Daniel Hochmuth <sleepy.locust@gmail.com>
2023-04-14 17:25:59 +0200

    test revert

--- Assets/Scripts/StartMenuHandler.cs
+++ Assets/Scripts/StartMenuHandler.cs
@@ -7,12 +7,10 @@ using TMPro;
 
 public class StartMenuHandler : MonoBehaviour
 {
-    public TMP_InputField usernameInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        usernameInput.onEndEdit.AddListener(delegate{ProcessInput(usernameInput);});
     }
 
     // Update is called once per frame
@@ -26,9 +24,9 @@ public class StartMenuHandler : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    void ProcessInput(TMP_InputField input)
+    public void UsernameEntered(string username)
     {
-        MainManager.ManagerInstance.username = input.text;
+        MainManager.ManagerInstance.username = username;
     }
 
 }

71e22d3b5ec2abd60e2b05d59efbbafa3df9d7f1
Daniel Hochmuth <sleepy.locust@gmail.com>
2023-04-14 17:26:04 +0200

    Revert "test revert"
    This reverts commit 08e97309fc25940e31c21142687a73d9f635aa02.

--- Assets/Scripts/StartMenuHandler.cs
+++ Assets/Scripts/StartMenuHandler.cs
@@ -7,10 +7,12 @@ using TMPro;
 
 public class StartMenuHandler : MonoBehaviour
 {
+    public TMP_InputField usernameInput;
 
     // Start is called before the first frame update
     void Start()
     {
+        usernameInput.onEndEdit.AddListener(delegate{ProcessInput(usernameInput);});
     }
 
     // Update is called once per frame
@@ -24,9 +26,9 @@ public class StartMenuHandler : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    public void UsernameEntered(string username)
+    void ProcessInput(TMP_InputField input)
     {
-        MainManager.ManagerInstance.username = username;
+        MainManager.ManagerInstance.username = input.text;
     }
 
 }
